f70fca6bca4b8b8cad1a7b49dcc27dec85f31376
Apix123 <144084614+Apix123@users.noreply.github.com>
2025-12-23 22:58:42 -0600

    CRUD Alumnos
    Ya agrega y visualizas alumnos, en la lista, y sale del menu con la opcion 3.

---
+++ Alumno.java
@@ -0,0 +1,20 @@
+public class Alumno { //--Clase Alumno para declarar variables a requerir para el CRUD
+    private int id; 
+    private String name; 
+    private int age;
+
+//--Declaro Constructor para almacenar los datos de las variables.
+public Alumno(int id, String name, int age) {
+    this.id = id; 
+    this.name = name; 
+    this.age = age; 
+}
+
+@Override //--indicando que esta sobreescribiendo
+public String toString() {
+    return "ID: " + id + "\n"
+         + "Nombre: " + name + "\n" 
+         + "Edad: " + age + "\n";
+}
+
+}
--- Main.java
+++ Main.java
@@ -1,32 +1,54 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
+
     public static void main(String [] args){
+
+    Scanner scanner = new Scanner(System.in);//--Declaro un objeto escaner para leer entradas.
+    ArrayList<Alumno> lista_alumnos = new ArrayList<>();//--Se crea el Array llamado lista_alumno basado de la clase Alumno.
+    boolean salir = false;
+
+    while(!salir){
+
         System.out.println("Menu CRUD");//--Escribo el menu para visualizarlo en la consola.
         System.out.println("1. Agregar Alumno");
         System.out.println("2. Mostrar Alumno");
         System.out.println("3. Salir");
         System.out.println("Elegir una opcion: ");
 
-        Scanner scanner = new Scanner(System.in);//--Declaro un objeto escaner para leer entradas.
         int opcion = scanner.nextInt(); //---Declaro variable para que se guarde el valor declarado por el usuario.
 
         System.out.println("Elegiste la opcion: " + opcion);//--Mensaje donde indica que opcion elegiste
-        
+
         switch (opcion){//--Declarando los mensajes a mostrar dependiendo de la opcion a escribir.
             case 1:
-                System.out.println("Agregar Alumno");
-                ArrayList<String> Agregar = ArrayList<>();
-                Agregar.add()
+                System.out.println("Agregar Datos de Alumno");//--Muestro el mensaje para indicar que se agregara alumnos.
+
+                System.out.println("Escriba ID: ");//--Imprimo el mensaje para pedir datos.
+                int id = scanner.nextInt();//--Declaro la variable de la clase Alumno para leer parametro enteros.
+
+                scanner.nextLine();//--Limpia buffer para el salto de linea.
+
+                System.out.println("Escriba Nombre: ");
+                String name = scanner.nextLine();
+
+                System.out.println("Escriba Edad: ");
+                int age = scanner.nextInt();
+
+                Alumno alumno = new Alumno(id, name, age);//Declaro la variable(alumno), donde almacena los que se leyo anteriormente(id, name, age).
+                lista_alumnos.add(alumno);//--Declaro el Array para almacenar la variable.
                 break;
             case 2:
-                System.out.println("Mostrar Alumnos");
+                System.out.println("Lista de Alumnos: ");
+                System.out.println(lista_alumnos);
                 break;
             case 3:
-                System.out.println("Salir del Sistema");
+                System.out.println("Saliendo del Sistema");
+                salir = true;
                 break;
             default:
                 System.out.println("Opcion Invalida");
+            }
         }
     }
 }
